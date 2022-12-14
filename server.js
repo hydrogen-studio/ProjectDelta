f29d61b89c6962f90c4178344d7c9097769452b0
hydrogen-studio <hydrogen.studio.llc@gmail.com>
2022-12-13 23:57:30 -0800

    first commit

--- server.js
+++ server.js
@@ -205,7 +205,7 @@ app.get('/linked-role', async (req, res) => {
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
