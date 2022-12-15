ceb8afe82b5e7e46d0312599ba9e5d1e6d68c68c
hydrogen-studio <hydrogen.studio.llc@gmail.com>
2022-12-14 21:10:39 -0800

    updated server tech

--- server.js
+++ server.js
@@ -158,6 +158,18 @@ function getOAuthUrl() {
    res.send('👋');
  });
 
+ app.get('/update-commands', async (req, res) => {
+  const { url, state } = getOAuthUrl();
+
+  // Store the signed state param in the user's cookies so we can verify
+  // the value later. See:
+  // https://discord.com/developers/docs/topics/oauth2#state-and-security
+  res.cookie('clientState', state, { maxAge: 1000 * 60 * 5, signed: true });
+
+  // Send the user to the Discord owned OAuth2 authorization endpoint
+  res.redirect(url);
+});
+
 app.get('/linked-role', async (req, res) => {
   const { url, state } = getOAuthUrl();
 
