e30a8d832f460e12b01962d1d81a7685f70f2433
hydrogen-studio <hydrogen.studio.llc@gmail.com>
2022-12-21 09:42:58 -0800

    added lastfm

--- storage.js
+++ storage.js
@@ -2,7 +2,6 @@ let fs = require('fs');
 const store = new Map();
 var JSONStore = require('json-store');
 var db = JSONStore(__dirname + '/database.json');
-var premiumdb = JSONStore(__dirname + '/premium.json');
 var lfmdb = JSONStore(__dirname + '/lfm.json');
 exports.storeDiscordTokens = async function (userId, tokens) {
 	await store.set(`discord-${userId}`, tokens);
@@ -19,15 +18,6 @@ exports.commandsGet = async function(userId) {
     let file = JSON.parse(fs.readFileSync(__dirname + '/database.json').toString("utf8"));
 	return file[`commands-${userId}`] == undefined ? 0 : file[`commands-${userId}`];
 }
-
-exports.premiumSet = async function (userId, data) {
-	await premiumdb.set(`premium-${userId}`, JSON.stringify(data));
-}
-exports.premiumGet = async function(userId) {
-    let file = JSON.parse(fs.readFileSync(__dirname + '/premium.json').toString("utf8"));
-	return file[`premium-${userId}`] == undefined ? 0 : JSON.parse(file[`premium-${userId}`]);
-}
-
 
 exports.lastfmSet = async function (userId, data) {
 	await lfmdb.set(`lfmuser-${userId}`, JSON.stringify(data));
